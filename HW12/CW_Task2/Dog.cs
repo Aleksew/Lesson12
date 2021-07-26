40f2bba494ab739f00cad84bdf944cd7047d7e49
Aleksew <aleksewww@gmail.com>
2021-07-26 22:52:47 +0300

    Comit2

---
+++ HW12/CW_Task2/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Task2
+{
+    public delegate EventHandler<OrderEventArgs> OrderToDog();
+    public class Dog
+    {
+        public string Order { get; set; }
+
+        public void OrderHandler(object sender, OrderEventArgs e)
+        {
+            Console.WriteLine(e.GetType().Name );
+
+            if ( true)//e.GetType
+            {
+                Console.WriteLine($"Я выполняю команду {e.Order}");
+                Order = e.Order;
+            }
+            else
+            {
+                Console.WriteLine("Подозрительно смотрю на постороннего");
+            }
+        }
+    }
+}
---
+++ HW12/CW_Task2/Owner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Task2
+{
+    class Owner
+    {
+        public event EventHandler<OrderEventArgs> OrderToDog = null;
+
+        public void GiveAnOrder(string str)
+        {
+            var temp = new OrderEventArgs();
+            temp.Order = str;
+        }
+        public void InvokeEvent(object sender, OrderEventArgs e)
+        {
+            OrderToDog.Invoke(sender, e);
+        }
+    }
+}
